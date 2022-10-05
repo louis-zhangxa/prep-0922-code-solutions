b5731a5a514634975f9e93512c4a71ea47317eda
“Louis <Louisgeemod@gmail.com>
2022-10-05 07:42:41 +0000

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('language array', languages);
+
+var firstElement = languages[0];
+console.log('the first of the array is', firstElement);
+
+var thirdElement = languages[2];
+console.log('the 3rd element of the array is', thirdElement);
+
+var length = languages.length;
+console.log('The length of the arrahy is', length);
+
+var lastElement = languages[languages.length - 1];
+console.log('The last element of the array is', lastElement);
