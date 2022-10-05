2671db112be757c8c268ec42855b97ffc7ca05c1
“Louis <Louisgeemod@gmail.com>
2022-10-05 09:45:10 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+var number = -2;
+for (var i = 0; i < 10; i++) {
+  number += 2;
+  console.log(number);
+}
+
+for (var i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i + '!');
+}
